cb041e58cdcb9331c00bd380b2f407fa574be079
Eddie Bickham <bickhame@gmail.com>
2017-10-07 06:18:34 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,7 +2,7 @@ var array = [];
 function() {
   for (let i = 0;i < 25;i++) {
   if (i <= 1) {
-    return "I am " + i + " strange loop";
+    return array.push(i)"I am " + i + " strange loop";
   }
   else {
     return " I am " + ${i} + " strange loops"
